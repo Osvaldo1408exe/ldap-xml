11ac5d973a1e01c6cbb73ba448221192f615c489
Osvaldo1408exe <Osvaldo1408.exe@gmail.com>
2025-04-07 15:48:05 -0300

    fix: regex

--- src/main/java/com/osvaldo/ldapxml/controller/UsuarioController.java
+++ src/main/java/com/osvaldo/ldapxml/controller/UsuarioController.java
@@ -1,12 +1,17 @@
 package com.osvaldo.ldapxml.controller;
 
+import com.osvaldo.ldapxml.model.Usuario;
 import com.osvaldo.ldapxml.service.UsuarioService;
+import com.osvaldo.ldapxml.service.XmlUsuarioReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -16,17 +21,15 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping("/importar")
-    public ResponseEntity<String> importar(@RequestParam("file") MultipartFile file) {
-        try {
-            File tempFile = File.createTempFile("usuarios", ".xml");
-            file.transferTo(tempFile);
-
-            usuarioService.importarUsuarios(tempFile);
-
-            return ResponseEntity.ok("Usuários importados com sucesso!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body("Erro: " + e.getMessage());
+    public ResponseEntity<List<Usuario>> importarUsuarios(@RequestParam("file") MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            List<Usuario> usuarios = XmlUsuarioReader.lerUsuarios(inputStream);
+            return ResponseEntity.ok(usuarios);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+
+
 }
