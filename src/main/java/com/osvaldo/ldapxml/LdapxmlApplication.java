0b7bb0f31e14e0b413cbec1d55ecc66ab6e4da55
Osvaldo1408exe <Osvaldo1408.exe@gmail.com>
2025-04-07 14:04:15 -0300

    initial commit

---
+++ src/main/java/com/osvaldo/ldapxml/LdapxmlApplication.java
@@ -0,0 +1,13 @@
+package com.osvaldo.ldapxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapxmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osvaldo/ldapxml/LdapxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osvaldo.ldapxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
