a313a0890356c4bfab3c31622d798425c0089833
Osvaldo1408exe <Osvaldo1408.exe@gmail.com>
2025-04-11 14:55:22 -0300

    feat: CRUD created

--- src/main/java/com/osvaldo/ldapxml/controller/XmlImportController.java
+++ src/main/java/com/osvaldo/ldapxml/controller/XmlImportController.java
@@ -1,6 +1,6 @@
 package com.osvaldo.ldapxml.controller;
 
-import com.osvaldo.ldapxml.config.LdapConnection;
+import com.osvaldo.ldapxml.service.XmlService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +13,44 @@ import java.io.InputStream;
 @RequestMapping("/api/xml")
 public class XmlImportController {
 
-    private final LdapConnection ldapService;
+    private final XmlService XmlService;
 
-    public XmlImportController(LdapConnection ldapService) {
-        this.ldapService = ldapService;
+    public XmlImportController(XmlService XmlService) {
+        this.XmlService = XmlService;
     }
 
-    @PostMapping("/importar")
+    //ADD USER OR GROUP
+    @PostMapping("/ImportEntity")
+    public ResponseEntity<String> importXml(@RequestParam("file") MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            XmlService.addXml(inputStream);
+            return ResponseEntity.ok("XML processado com sucesso.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao processar o XML: " + e.getMessage());
+        }
+    }
+
+    //MODIFY USER
+    @PutMapping("/ImportEntity")
     public ResponseEntity<String> importarXml(@RequestParam("file") MultipartFile file) {
         try (InputStream inputStream = file.getInputStream()) {
-            ldapService.processXml(inputStream);
-            return ResponseEntity.ok("XML processado com sucesso.");
+            XmlService.modifyUser(inputStream);
+            return ResponseEntity.ok("Usuário alterado com sucesso.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao processar o XML: " + e.getMessage());
+        }
+    }
+
+    //DELETE USER
+    @DeleteMapping("/ImportEntity")
+    public ResponseEntity<String> deletarEntidade(@RequestParam("file") MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            XmlService.deleteXml(inputStream);
+            return ResponseEntity.ok("Entidade apagada com sucesso.");
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
